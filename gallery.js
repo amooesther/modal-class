dfd9f4aef9a14e3e03a87ca7ceeaf4bef7991b02
Esther Toyin <amooesther2503@gmail.com>
2023-12-16 10:44:15 +0000

    second commit

--- gallery.js
+++ gallery.js
@@ -13,8 +13,19 @@ const imageList=[
     {
         imageName:"imageThree",
         imageLink:"https://images.pexels.com/photos/17414711/pexels-photo-17414711/free-photo-of-woman-in-a-red-dress-sitting-on-the-steps.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+    },
+    {
+        imageName:"imageFour",
+        imageLink:"https://images.pexels.com/photos/17984670/pexels-photo-17984670/free-photo-of-model-in-a-white-blouse-and-gray-long-skirt.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+    },
+    {
+        imageName:"imageFive",
+        imageLink:"https://images.pexels.com/photos/17825357/pexels-photo-17825357/free-photo-of-model-in-lace-cropped-top.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+    },
+    {
+        imageName:"imageSix",
+        imageLink:"https://images.pexels.com/photos/11634665/pexels-photo-11634665.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
     }
-
 ]
 
 const gallery= document.getElementById("gallery");
